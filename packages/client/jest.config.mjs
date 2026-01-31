ae2e43fe04b88a8eda9175d2eba7fbfb88396b25
Alexandra <varyandis@yandex.ru>
2026-01-31 16:00:37 +0300

    fix: lint

--- packages/client/jest.config.mjs
+++ packages/client/jest.config.mjs
@@ -1,7 +1,7 @@
-const dotenv = require('dotenv')
+import dotenv from 'dotenv'
 dotenv.config()
 
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -3,10 +3,11 @@ import { render } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
-// @ts-ignore
-global.fetch = jest.fn(() =>
-  Promise.resolve({ json: () => Promise.resolve('hey') })
+const fetchMock = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('hey') } as Response)
 )
+
+globalThis.fetch = fetchMock as unknown as typeof fetch
 
 test('App renders without crashing', () => {
   render(
