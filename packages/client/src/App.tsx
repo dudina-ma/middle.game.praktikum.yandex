88b29bd2466105f6cc5b6684a638d67521e875ae
Valeriy230591 <valeriybojko42@gmail.com>
2026-02-05 19:45:25 +0300

    fix-commit

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from './store'
 
-import { selectUser } from './slices/userSlice'
+import { fetchUserThunk, selectUser } from './slices/userSlice'
 
 const App = () => {
   const user = useSelector(selectUser)
--- packages/client/src/routes.tsx
+++ packages/client/src/routes.tsx
@@ -1,8 +1,14 @@
 import { AppDispatch, RootState } from './store'
-
-import { initMainPage, MainPage } from './pages/Main'
-import { initFriendsPage, FriendsPage } from './pages/FriendsPage'
-import { initNotFoundPage, NotFoundPage } from './pages/NotFound'
+import Layout from './components/Layout/Layout'
+import Main from './pages/Main/Main'
+import Login from './pages/Login/Login'
+import SignIn from './pages/SignIn/SignIn'
+import Profile from './pages/Profile/Profile'
+import Game from './pages/Game/Game'
+import Leaderboard from './pages/Leaderboard/Leaderboard'
+import Forum from './pages/Forum/Forum'
+import ForumTopic from './pages/ForumTopic/ForumTopic'
+import NotFound from './pages/NotFound/NotFound'
 
 export type PageInitContext = {
   clientToken?: string
@@ -14,20 +20,71 @@ export type PageInitArgs = {
   ctx: PageInitContext
 }
 
+// Общие заглушки
+export const createStubFetchData = (pageName: string) => {
+  return async (): Promise<void> => {
+    console.log(`Stub fetchData called for ${pageName} page`)
+    return Promise.resolve()
+  }
+}
+
+export const emptyFetchData = async (): Promise<void> => {
+  return Promise.resolve()
+}
+
 export const routes = [
   {
-    path: '/',
-    Component: MainPage,
-    fetchData: initMainPage,
+    path: '/login',
+    Component: Login,
+    fetchData: createStubFetchData('Login'),
   },
   {
-    path: '/friends',
-    Component: FriendsPage,
-    fetchData: initFriendsPage,
+    path: '/sign-in',
+    Component: SignIn,
+    fetchData: createStubFetchData('SignIn'),
   },
+
+  {
+    path: '/',
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        Component: Main,
+        fetchData: createStubFetchData('Main'),
+      },
+      {
+        path: 'profile',
+        Component: Profile,
+        fetchData: createStubFetchData('Profile'),
+      },
+      {
+        path: 'game',
+        Component: Game,
+        fetchData: createStubFetchData('Game'),
+      },
+      {
+        path: 'leaderboard',
+        Component: Leaderboard,
+        fetchData: createStubFetchData('Leaderboard'),
+      },
+      {
+        path: 'forum',
+        Component: Forum,
+        fetchData: createStubFetchData('Forum'),
+      },
+      {
+        path: 'forum/:topicId',
+        Component: ForumTopic,
+        fetchData: createStubFetchData('ForumTopic'),
+      },
+    ],
+    fetchData: createStubFetchData('Layout'),
+  },
+
   {
     path: '*',
-    Component: NotFoundPage,
-    fetchData: initNotFoundPage,
+    Component: NotFound,
+    fetchData: emptyFetchData,
   },
 ]
