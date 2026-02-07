66bf3e4c0406af166398aa295de685d6447d3f8b
Andrey Ardvenev <a.ardvenev@bi.zone>
2026-02-07 13:28:28 +0300

    Revert "Signin"
    This reverts commit 9d5dec84d1dd3aac3b4abd86dd7c3b94efdd897c.

--- packages/client/src/routes.tsx
+++ packages/client/src/routes.tsx
@@ -1,9 +1,8 @@
 import { AppDispatch, RootState } from './store'
 
 import { initMainPage, MainPage } from './pages/Main'
-import { FriendsPage, initFriendsPage } from './pages/FriendsPage'
+import { initFriendsPage, FriendsPage } from './pages/FriendsPage'
 import { initNotFoundPage, NotFoundPage } from './pages/NotFound'
-import { SignIn } from './organisms/SignIn/SignIn'
 
 export type PageInitContext = {
   clientToken?: string
@@ -25,11 +24,6 @@ export const routes = [
     path: '/friends',
     Component: FriendsPage,
     fetchData: initFriendsPage,
-  },
-  {
-    path: '/signin',
-    Component: SignIn,
-    fetchData: initMainPage,
   },
   {
     path: '*',
