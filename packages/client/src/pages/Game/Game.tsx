a6733e683917599708140cf583f9d15d758bcba9
Valeriy230591 <valeriybojko42@gmail.com>
2026-02-10 14:00:48 +0300

    Resolve/5.12-start-game-menu

--- packages/client/src/pages/Game/Game.tsx
+++ packages/client/src/pages/Game/Game.tsx
@@ -1,8 +1,14 @@
 import EndGame from '../../components/EndGame/EndGame'
+import StartGame from '../../components/StartGame/StartGame'
 import { useState } from 'react'
 const Game = () => {
-  const [gameOver] = useState(true)
+  const [gameOver] = useState(false)
 
-  return <div>{gameOver && <EndGame />}</div>
+  return (
+    <div>
+      {<StartGame />}
+      {gameOver && <EndGame />}
+    </div>
+  )
 }
 export default Game
