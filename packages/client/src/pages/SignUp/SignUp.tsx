b9e085a0c57fa46af214078d020fccae33089717
Andrey Ardvenev <a.ardvenev@bi.zone>
2026-02-12 15:38:04 +0300

    SignUp page

---
+++ packages/client/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,11 @@
+import { AuthPageContainer } from '../../atoms'
+import { SignUpForm } from '../../organisms/SignUpForm/SignUpForm'
+
+const SignUp = () => {
+  return (
+    <AuthPageContainer>
+      <SignUpForm />
+    </AuthPageContainer>
+  )
+}
+export default SignUp
