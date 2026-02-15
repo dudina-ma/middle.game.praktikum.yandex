0af97723346466de47039db2892338596e1b6fce
dudina-ma <dudina27maria@gmail.com>
2026-02-15 18:31:16 +0100

    Add simple SW

---
+++ packages/client/public/sw.js
@@ -0,0 +1,9 @@
+//this — это ServiceWorkerGlobalScope
+
+this.addEventListener("install", event => {
+    console.log("install");
+});
+
+this.addEventListener("activate", event => {
+    console.log("activate");
+});
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -40,3 +40,23 @@ if (hasSSRContent && hasInitialState) {
     </ErrorBoundary>
   )
 }
+
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope
+          )
+        })
+        .catch((error: Error) => {
+          console.log('ServiceWorker registration failed: ', error)
+        })
+    })
+  }
+}
+
+startServiceWorker()
