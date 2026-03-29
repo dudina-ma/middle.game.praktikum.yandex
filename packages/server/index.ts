10771f2425fa7913afbd5295389462870a15522b
Andrey Ardvenev <a.ardvenev@bi.zone>
2026-03-29 21:00:41 +0300

    index.ts
    Optimize server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,68 +1,29 @@
 import 'reflect-metadata'
 import dotenv from 'dotenv'
-import cors from 'cors'
+import { connectDatabase, syncDatabase } from './sequelize'
+import * as process from 'node:process'
+import { app } from './app'
+
 dotenv.config()
 
-import express from 'express'
-import { ValidationError } from 'sequelize'
-import { syncSequelizeModels, testSequelizeConnection } from './sequelize'
-import commentsRouter from './routes/commentsRouter'
-import repliesRouter from './routes/repliesRouter'
-import topicsRouter from './routes/topicsRouter'
-import reactionsRouter from './routes/reactionsRouter'
-
-const app = express()
-app.use(cors())
-app.use(express.json({ limit: '1mb' }))
 const port = Number(process.env.SERVER_PORT) || 3001
 
-app.use('/api', commentsRouter)
-app.use('/api', repliesRouter)
-app.use('/api', reactionsRouter)
-app.use('/api/topics', topicsRouter)
-
-app.get('/health', (_, res) => {
-  res.status(200).json({ status: 'ok' })
-})
-
-app.use(
-  (
-    err: unknown,
-    _req: express.Request,
-    res: express.Response,
-    _next: express.NextFunction
-  ) => {
-    console.error(err)
-    if (err instanceof ValidationError) {
-      res.status(400).json({
-        message: 'Некорректные данные',
-        errors: err.errors.map(e => ({
-          message: e.message,
-          path: e.path,
-          value: e.value,
-        })),
-      })
-      return
-    }
-    res.status(500).json({ message: 'Внутренняя ошибка сервера' })
-  }
-)
-
-const initializeServer = async (): Promise<void> => {
-  try {
-    await testSequelizeConnection()
-    await syncSequelizeModels()
-
-    app.listen(port, () => {
-      console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-    })
-  } catch (error) {
-    console.error(
-      'Failed to connect to database. Server will not start.',
-      error
-    )
-    process.exit(1)
-  }
+const onSyncError = (error: unknown) => {
+  console.error('Failed to sync database. Server will not start', error)
 }
 
-void initializeServer()
+const onAuthError = (error: unknown) => {
+  console.error('Failed to connect to database. Server will not start', error)
+}
+
+const onServerStart = () => {
+  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+}
+
+connectDatabase()
+  .then(() => {
+    syncDatabase()
+      .then(() => app.listen(port, onServerStart))
+      .catch(onSyncError)
+  })
+  .catch(onAuthError)
